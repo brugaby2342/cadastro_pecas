82add529c367073fb914a2eafe500c1fd17d03d8
Bruna Gabriela Ribeiro Sartor <brunagabrielaribeirosartor@Mac.local>
2026-04-06 20:01:58 -0300

    definindo as tarefas

---
+++ main.py
@@ -0,0 +1,20 @@
+#Receber dados para cadastrar peças:
+#id
+#peso
+#cor
+#comprimento
+
+#avaliar se está aprovada ou reprovada, sob os seguintes critérios de qualidade:
+#Peso entre 95 kg e 105 kg
+#cor azul ou verde
+#comprimento entre 10 cm e 20 cm
+
+#armazenar as peças em caixas (10 peças por caixa)
+#quando alcancar as 10 peças, fechar a caixa e abrir uma nova
+
+#Relatórios:
+#Total de peças aprovadas
+#Total de peças reprovadas, com o motivo da reprovação
+#Quantidade de caixas
+
+
